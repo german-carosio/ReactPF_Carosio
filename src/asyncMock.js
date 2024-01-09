b07fff8308d7935894932b4aecfb208357287803
German Carosio <carosiogerman@gmail.com>
2024-01-09 20:07:37 -0300

    se agregaron botones para ordenar y más productos

--- src/asyncMock.js
+++ src/asyncMock.js
@@ -2,7 +2,7 @@ const products = [
     {
         id:1,
         name: 'Epipremnum aureum',
-        price: 100,
+        price: 200,
         category: 'interior',
         img: 'https://i.pinimg.com/564x/9a/48/31/9a48312ba98581237962e4eb8b9a812a.jpg',
         stock: 7,
@@ -21,8 +21,8 @@ const products = [
 
     {
         id:3,
-        name: 'Crassula ovata',
-        price: 130,
+        name: 'Haworthia attenuata',
+        price: 500,
         category: 'suculentas',
         img: 'https://i.pinimg.com/564x/fa/3d/4b/fa3d4bdef9713f1e6fa862e533021e29.jpg',
         stock: 10,
@@ -32,7 +32,7 @@ const products = [
     {
         id:4,
         name: 'Dracaena fragans',
-        price: 200,
+        price: 600,
         category: 'interior',
         img: 'https://i.pinimg.com/564x/06/e6/f8/06e6f886fb90d88a7dc85517526b1b18.jpg',
         stock: 10,
@@ -42,7 +42,7 @@ const products = [
     {
         id:5,
         name: 'Chamaedorea elegans',
-        price: 130,
+        price: 500,
         category: 'interior',
         img: 'https://i.pinimg.com/564x/50/64/e2/5064e2d07fdf7e59303c33a637030034.jpg',
         stock: 10,
@@ -52,7 +52,7 @@ const products = [
     {
         id:6,
         name: 'Sedum morganianum',
-        price: 130,
+        price: 400,
         category: 'suculenta',
         img: 'https://i.pinimg.com/564x/67/c4/d3/67c4d3d464f824b6185cea4ae4c7b4d8.jpg',
         stock: 10,
@@ -62,7 +62,7 @@ const products = [
     {
         id:7,
         name: 'Ficus pandurata',
-        price: 130,
+        price: 700,
         category: 'interior',
         img: 'https://i.pinimg.com/564x/d8/a6/9f/d8a69f535ee9804c16d7246d40922d71.jpg',
         stock: 10,
@@ -72,7 +72,7 @@ const products = [
     {
         id:8,
         name: 'Nephrolepis',
-        price: 130,
+        price: 800,
         category: 'interior',
         img: 'https://i.pinimg.com/564x/b2/d4/11/b2d4118ef93ac99be62bdd1729d03647.jpg',
         stock: 10,
@@ -82,7 +82,7 @@ const products = [
     {
         id:9,
         name: 'Sansevieria trifasciata',
-        price: 130,
+        price: 100,
         category: 'suculentas',
         img: 'https://i.pinimg.com/564x/ec/20/c8/ec20c8d442ee84ac9660ec7c76282f49.jpg',
         stock: 10,
@@ -93,7 +93,7 @@ const products = [
     {
         id:10,
         name: 'Crassula ovata',
-        price: 130,
+        price: 279,
         category: 'suculenta',
         img: 'https://i.pinimg.com/564x/66/0c/6e/660c6e06c7487c5cf42b30633fcd1f93.jpg',
         stock: 10,
@@ -103,7 +103,7 @@ const products = [
     {
         id:11,
         name: 'Echeveria elegans',
-        price: 130,
+        price: 300,
         category: 'suculentas',
         img: 'https://i.pinimg.com/564x/8c/6b/cb/8c6bcb39cffe7af3f5d001e925fc5d38.jpg',
         stock: 10,
@@ -113,7 +113,7 @@ const products = [
     {
         id:12,
         name: 'Aloe Vera',
-        price: 130,
+        price: 400,
         category: 'suculentas',
         img: 'https://i.pinimg.com/564x/bc/b8/0e/bcb80eecfa67ddf8b80d1ed7e39030b7.jpg',
         stock: 10,
@@ -123,7 +123,7 @@ const products = [
     {
         id:13,
         name: 'Geranio',
-        price: 130,
+        price: 500,
         category: 'exterior',
         img: 'https://i.pinimg.com/564x/60/f6/4e/60f64e80089dba9910d46ec6e7d2d67a.jpg',
         stock: 10,
@@ -133,7 +133,7 @@ const products = [
     {
         id:14,
         name: 'Chlorophytum comosum',
-        price: 130,
+        price: 150,
         category: 'exterior',
         img: 'https://i.pinimg.com/564x/2a/16/97/2a1697c9471d0a3f2117820fca4d3ec3.jpg',
         stock: 10,
@@ -143,7 +143,7 @@ const products = [
     {
         id:15,
         name: 'Chlorophytum comosum',
-        price: 130,
+        price: 700,
         category: 'exterior',
         img: 'https://i.pinimg.com/564x/35/f9/0f/35f90fc45c9ce84dd6bf2d687393edfb.jpg',
         stock: 10,
@@ -153,9 +153,39 @@ const products = [
     {
         id:16,
         name: 'Monstera deliciosa',
-        price: 150,
+        price: 250,
         category: 'interior',
         img: 'https://i.pinimg.com/564x/71/57/21/715721cef5bcf33a255a0b5480e27d1e.jpg',
+        stock: 10,
+        description: 'Costilla de Adán'
+    },
+
+    {
+        id:17,
+        name: 'Mammillaria gracilis',
+        price: 186,
+        category: 'suculentas',
+        img: 'https://i.pinimg.com/564x/37/37/cc/3737cce097011b3a14ace59cb3ddb7fb.jpg',
+        stock: 10,
+        description: 'Costilla de Adán'
+    },
+
+    {
+        id:18,
+        name: 'Astrophytum asterias',
+        price: 115,
+        category: 'suculentas',
+        img: 'https://i.pinimg.com/564x/d3/eb/0d/d3eb0d4995abc75591e922eca31bb150.jpg',
+        stock: 10,
+        description: 'Costilla de Adán'
+    },
+
+    {
+        id:19,
+        name: 'Hedera helix',
+        price: 115,
+        category: 'exterior',
+        img: 'https://i.pinimg.com/564x/17/c0/41/17c041f557843079ece993b8d2b83ade.jpg',
         stock: 10,
         description: 'Costilla de Adán'
     }
@@ -182,6 +212,14 @@ export const getProductByCategory = (categoryId)=> {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve(products.filter(prod=> prod.category === categoryId))
+        }, 500);
+    })
+}
+
+export const menMay = (products)=> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(products.sort(((a,b)=> a.price - b.price)))
         }, 500);
     })
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -4,6 +4,10 @@ import styles from './ItemListContainer.module.css'
 import { getProductByCategory, getProducts } from '../../asyncMock'
 import { useParams } from 'react-router-dom'
 
+//https://www.youtube.com/watch?v=OGwPjSxm1lU paginación
+
+
+//Primera letra mayuscula
 function mayus(text) {
   const firstLetter = text.charAt(0);
   const rest = text.slice(1);
@@ -12,6 +16,7 @@ function mayus(text) {
 
 const ItemListContainer = () => {
   const [products, setProducts] = useState([])
+  const [productsCopy, setProductsCopy] = useState([])
 
   const {categoryId} = useParams()
 
@@ -22,15 +27,19 @@ const ItemListContainer = () => {
     asyncFunc(categoryId)
     .then(response =>{
         setProducts(response)
+        setProductsCopy(response)
     })
     .catch(error =>{
       console.log(error)
     })
 }, [categoryId])
-
+  
   return (
     <>  
       <div className={styles.container}>
+      <button onClick={()=>/*fun orden*/{setProducts([].concat(products).sort((a,b)=> a.price - b.price))}}>Menor precio</button>
+      <button onClick={()=>{setProducts([].concat(products).sort((a,b)=> b.price - a.price))}}>Mayor precio</button>
+      <button onClick={()=>{setProducts(productsCopy)}}>Quitar filtros</button>
       <h3>{mayus(categoryId ? categoryId : 'Todas las plantas')}</h3>
       <ItemList products={products}/>
       </div>
