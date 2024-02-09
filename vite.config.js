92103234b270c7458813cd10d684a5fc25ec1e5a
German Carosio <carosiogerman@gmail.com>
2024-02-08 23:53:31 -0300

    probando vercel numero mil

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    modules: {
+      // Habilita CSS Modules
+      generateScopedName: '[name]__[local]___[hash:base64:5]'
+    }
+  }
+});
