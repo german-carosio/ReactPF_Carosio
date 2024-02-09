298b4c4c9d85e393a2f76fdce9d641da01396546
German Carosio <carosiogerman@gmail.com>
2024-02-08 23:44:01 -0300

    probando vercel por quinta vez

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,34 +1,34 @@
 import React from 'react'
 import logo from '../../assets/logo.png'
-import styles from './NavBar.module.css'
+import classes from './NavBar.module.css'
 import CartWidget from '../CartWidget/CartWidget'
 import { Link, NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <>
-      <nav className={styles.container}>
+      <nav className={classes.container}>
         <Link to={'/'}>
-          <img className={styles.logo} src={logo} alt="imagen logo" />
+          <img className={classes.logo} src={logo} alt="imagen logo" />
         </Link>
         
-        <ul className={styles.menu}>
-          <li className={styles.menuItem}><NavLink to={`/`} className={styles.menuLink}>Todos</NavLink></li>
-          <li className={styles.menuItem}><NavLink to={`/category/interior`} className={styles.menuLink}>interior</NavLink></li>
-          <li className={styles.menuItem}><NavLink to={`/category/exterior`} className={styles.menuLink}>Exterior</NavLink></li>
-          <li className={styles.menuItem}><NavLink to={`/category/suculentas`} className={styles.menuLink}>Suculentas</NavLink></li>
+        <ul className={classes.menu}>
+          <li className={classes.menuItem}><NavLink to={`/`} className={classes.menuLink}>Todos</NavLink></li>
+          <li className={classes.menuItem}><NavLink to={`/category/interior`} className={classes.menuLink}>interior</NavLink></li>
+          <li className={classes.menuItem}><NavLink to={`/category/exterior`} className={classes.menuLink}>Exterior</NavLink></li>
+          <li className={classes.menuItem}><NavLink to={`/category/suculentas`} className={classes.menuLink}>Suculentas</NavLink></li>
         </ul>
 
-        <div className={styles.derecha}>
+        <div className={classes.derecha}>
 
-          <div className={styles.user}>
+          <div className={classes.user}>
             
           <span className="material-symbols-outlined">
             account_circle
           </span>
           </div>
 
-          <Link className={styles.cart} to={`/cart`}><CartWidget /></Link>
+          <Link className={classes.cart} to={`/cart`}><CartWidget /></Link>
           
         </div>
 
